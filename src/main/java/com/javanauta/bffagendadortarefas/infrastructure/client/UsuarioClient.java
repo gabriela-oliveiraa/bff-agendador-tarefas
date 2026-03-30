5423b1dba60f7344c963720aee66c5408f3151a9
Gabriela Oliveira <gabiiholiveiraa04@gmail.com>
2026-03-30 11:03:34 -0300

    Adicionado endpoint para busca de CEP

--- src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/UsuarioClient.java
@@ -8,6 +8,7 @@ import com.javanauta.bffagendadortarefas.business.dto.in.UsuarioDTORequest;
 import com.javanauta.bffagendadortarefas.business.dto.out.EnderecoDTOResponse;
 import com.javanauta.bffagendadortarefas.business.dto.out.TelefoneDTOResponse;
 import com.javanauta.bffagendadortarefas.business.dto.out.UsuarioDTOResponse;
+import com.javanauta.bffagendadortarefas.business.dto.out.ViaCepDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
@@ -49,4 +50,7 @@ public interface UsuarioClient {
     @PostMapping("/telefone")
     TelefoneDTOResponse cadastraTelefone(@RequestBody TelefoneDTORequest telefoneDTO,
                                          @RequestHeader("Authorization") String token);
+
+    @GetMapping("/endereco/{cep}")
+    ViaCepDTOResponse buscarDadosCep(@PathVariable("cep") String cep);
 }
